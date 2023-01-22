bf3627ad6fbbbe9c01710f596bb6b2cc7c639df9
vadim <vekww2@yandex.ru>
2023-01-22 04:12:00 +0300

    new

---
+++ scheduler/src/main/java/rsatu/resource/HeaderScheduleResource.java
@@ -0,0 +1,60 @@
+package rsatu.resource;
+
+import rsatu.pojo.HeaderSchedule;
+import rsatu.service.HeaderScheduleService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/headerschedules")
+public class HeaderScheduleResource {
+
+    @Inject
+    HeaderScheduleService shs;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getHeaderSchedules")
+    @RolesAllowed({"watchTH"})
+    public Response getHeaderSchedules(){
+        return Response.ok(shs.getHeaderSchedules()).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id_header_schedule}")
+    public Response getHeaderScheduleById(@PathParam("id_header_schedule") Long id_header_schedule){
+        return Response.ok(shs.getHeaderScheduleById(id_header_schedule)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/insertHeaderSchedule")
+    @RolesAllowed({"editHeader"})
+    public Response insertHeaderSchedule(HeaderSchedule tr){
+        return Response.ok(shs.insertHeaderSchedule(tr)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/updateHeaderSchedule")
+    @RolesAllowed({"editHeader"})
+    public Response updateHeaderSchedule(HeaderSchedule tr){
+        return Response.ok(shs.updateHeaderSchedule(tr)).build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id_header_schedule}")
+    @RolesAllowed({"editHeader"})
+    public Response deleteHeaderSchedule(@PathParam("id_header_schedule") Long id_header_schedule){
+        shs.deleteHeaderSchedule(id_header_schedule);
+        return Response.ok().build();
+    }
+
+}
---
+++ scheduler/src/main/java/rsatu/resource/SectionResource.java
@@ -0,0 +1,69 @@
+package rsatu.resource;
+
+import rsatu.service.SectionService;
+import rsatu.pojo.Section;
+import rsatu.service.SectionService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/sections")
+public class SectionResource {
+
+    @Inject
+    SectionService ss;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getSections")
+    @RolesAllowed({"watchSSL"})
+    public Response getSections(){
+        return Response.ok(ss.getSections()).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getSections1")
+    public Response getSections1(){
+        return Response.ok(ss.getSections1()).build();
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id_section}")
+    public Response getSectionById(@PathParam("id_section") Long id_section){
+        return Response.ok(ss.getSectionById(id_section)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/insertSection")
+    @RolesAllowed({"editSection"})
+    public Response insertSection(Section tr){
+        return Response.ok(ss.insertSection(tr)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/updateSection")
+    @RolesAllowed({"editSection"})
+    public Response updateSection(Section tr){
+        return Response.ok(ss.updateSection(tr)).build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id_section}")
+    @RolesAllowed({"editSection"})
+    public Response deleteSection(@PathParam("id_section") Long id_section){
+        ss.deleteSection(id_section);
+        return Response.ok().build();
+    }
+
+}
