71420f5ec5a0f120af49fd41ab151815b1be0b52
vadim47 <vekww5@gmail.com>
2023-02-07 04:11:51 +0300

    new

--- db_/src/main/java/ru/rgatu/dto/PlaceDTO.java
+++
@@ -1,9 +0,0 @@
-package ru.rgatu.dto;
-
-import lombok.Value;
-
-@Value
-public class PlaceDTO {
-    Long id_place;
-    String place_name;
-}
--- db_/src/main/java/ru/rgatu/dto/TrainerDTO.java
+++
@@ -1,8 +0,0 @@
-package ru.rgatu.dto;
-
-import lombok.Value;
-
-@Value
-public class TrainerDTO extends PeopleDTO {
-    Long id_trainer;
-}
